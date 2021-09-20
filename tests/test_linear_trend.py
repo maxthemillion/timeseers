49490063d762506cb68fd5f37966f2a339495377
maxthemillion <max.rahn@tum.de>
2021-09-20 22:04:34 +0200

    adding functional test

--- tests/test_linear_trend.py
+++ tests/test_linear_trend.py
@@ -12,3 +12,14 @@ def test_can_fit_generated_data(trend_data):
     model_delta = np.mean(model.trace_[model._param_name("delta")], axis=(0, 1)) * y_scale_factor
 
     np.testing.assert_allclose(model_delta, true_delta, atol=0.01)
+
+
+def test_can_fit_at_all(any_data):
+    data, _, n_changepoints = any_data
+    model = LinearTrend(n_changepoints=n_changepoints)
+    model.fit(data, data["value"], tune=1, draws=1)
+
+    y_scale_factor = model._y_scaler_.std_
+    model_delta = np.mean(model.trace_[model._param_name("delta")], axis=(0, 1)) * y_scale_factor
+
+    assert(model_delta is not None)
